6cbecf11514f668c3022bb31dfff33fda0844db2
Carolina Cobo <ccobo.dev@gmail.com>
2022-01-29 18:24:39 +0000

    Fix burger icon

--- components/Nav/Nabvar.js
+++ components/Nav/Nabvar.js
@@ -8,7 +8,7 @@ export default function Navbar() {
         <>
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div className="flex justify-between h-16">
-              <div>
+              <div className="hidden md:block">
                 <p></p>
               </div>
               <div className="flex">
