14154df8ad27eae608c652aace894605888807b3
Carolina Cobo <ccobo.dev@gmail.com>
2022-01-31 08:48:44 +0000

    Add meta file

---
+++ components/meta.js
@@ -0,0 +1,41 @@
+import Head from "next/head";
+
+export default function Meta() {
+  return (
+    <Head>
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon/favicon-32x32.png"
+      />
+      <link rel="shortcut icon" href="/favicon/favicon.ico" />
+      <meta name="msapplication-TileColor" content="#000000" />
+      <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
+      <meta name="theme-color" content="#000" />
+      <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
+      <meta
+        name="description"
+        content={`Projects and articles worked on as software engineer and career switcher.`}
+        key="description"
+      />
+      <meta
+        name="og:description"
+        content={`Projects and articles worked on as software engineer and career switcher.`}
+        key="og:description"
+      />
+      <meta
+        name="og:title"
+        content={`Carolina Cobo - Portfolio`}
+        key="og:title"
+      />
+      <meta
+        property="og:image"
+        content="/images/og/home-og.png"
+        key="og:image"
+      />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://www.carolinacobo.com/" />
+    </Head>
+  );
+}
