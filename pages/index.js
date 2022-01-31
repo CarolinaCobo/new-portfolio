63646c0a6b56c200909449989b8092b31b01eae7
Carolina Cobo <ccobo.dev@gmail.com>
2022-01-31 08:22:05 +0000

    Added meta tags

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Meta from "../components/meta";
 import Navbar from "../components/Nav/Nabvar";
 import HeroSection from "../components/HeroSection/HeroSection";
 import Projects from "../components/Projects/Projects";
@@ -10,9 +11,29 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Carolina Cobo Portfolio</title>
-        <meta name="description" content="Portfolio" />
+        <title>Carolina Cobo - Portfolio</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta
+          key="og:title"
+          property="og:title"
+          content="Carolina Cobo's career switcher portfolio to show projecs and articles"
+        />
+        <meta
+          key="og:image"
+          property="og:image"
+          content="/Users/carolinacobo/Projects/new-portfolio/public/assets/heroImage.svg"
+        />
+        <meta
+          key="og:description"
+          property="og:description"
+          content="Projects and articles worked on as software engineer and career switcher."
+        />
+        <meta
+          key="description"
+          property="description"
+          content="Projects and articles worked on as software engineer and career switcher."
+        />
+        <meta property="og:url" content="https://www.carolinacobo.com/" />
       </Head>
 
       <Navbar />
